6bcf1d8bf9f6f5c608a409257f13e1ac6ee2370d
Maia-Aedo <aedomaia10@gmail.com>
2022-04-11 10:58:30 -0300

    subo cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { EquipoComponent } from './equipo/equipo.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { CasosExitoComponent } from './casos-exito/casos-exito.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Inicio', component: InicioComponent},
+  {path: '', component: InicioComponent},
+  {path: 'equipo', component: EquipoComponent},
+  {path: 'servicios', component: ServiciosComponent},
+  {path: 'casos-exito', component: CasosExitoComponent},
+  {path: 'contacto', component: ContactoComponent},
+  {path: '**', component: InicioComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
